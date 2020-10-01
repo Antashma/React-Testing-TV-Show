db60241e44ff5dd62015c6bddc90c412231017e2
Samantha Goodman <slgoodman89@gmail.com>
2020-10-01 12:41:49 -0400

    moved api call to folder and app still fucntions

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
-import {render} from '@testing-library/react'
+import React from 'react';
+import App from './App';
+import {render} from '@testing-library/react';
 
 test('app renders', () => {
   render(<App />)
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import Episodes from './Episodes';
+
+test('component renders', () => {
+    episodesData = [ 
+        {}
+
+    ]
+    render(<Episodes episodes = {[]}/>)
+
+}) 
